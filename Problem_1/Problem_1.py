8c1b365da3ef3a46fb8f8603108d499028e05dda
topjer <60447803+topjer@users.noreply.github.com>
2020-02-01 14:44:09 +0100

    Problem 1 (#2)
    * Add files for Problem 1
    
    * Update Header
    
    Reformulated Header text
    
    * Refactor Code
    
    Instead of a loop, I decided to use a list comprehension. This allows me to solve the problem in one line.
    
    * Refactor Code
    
    Refactored the code yet again because I noticed sever performance issues when applied to big integers due to the fact that I iterate over an array of size n and then sum yet another array.
    
    Thus I decided to simply determine the number of dividers and use the gauss formula to compute the sum.
    
    Additionally, proper doc Strings have been added.
    
    * Update Test Cases
    
    Test cases for the gauss formula have been added.
    
    Also, I learned how to parametrize test cases so I did that to both test functions.

--- Problem_1/Problem_1.py
+++ Problem_1/Problem_1.py
@@ -7,5 +7,48 @@ multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 """
+def gauss_formula(number):
+    """Computes sum of all integers up to a given number
+
+    Parameters
+    ----------
+    number : int
+        Upper bound for summation
+
+    Returns
+    -------
+    int
+        Sum determined by the gauss formula
+
+    """
+    return number * (number + 1) // 2
+
 def sum_multiples_3_and_5(max_number):
-    return sum([number for number in range(1, max_number) if number % 3 == 0 or number % 5 == 0])
+    """Compute sum of all multiples of 3 and 5 smaller than a given number
+
+    In order to be more efficient the following steps are taken:
+     - take integer part of quotient of max_number and 3, this is the quantity of number that can be divided by 3
+     - determine the sum of those as 3 times the gauss sum for the quotient just mentioned
+     - do the same with 5
+     - add both sums
+     - take into consideration all numbers that can be divided by 3 and 5, i.e. fifteen; compute sum of all numbers that
+        can be divided by 15
+     - substract the sum for 15 from the previous sum
+
+    Parameters
+    ----------
+    max_number : int
+        Upper bound (excluding) to which numbers are inspected
+
+    Returns
+    -------
+    int
+        Sum of all numbers
+
+    """
+    divided_by_3 = (max_number - 1) // 3
+    divided_by_5 = (max_number - 1) // 5
+    divided_by_15 = (max_number - 1) // 15
+
+    return gauss_formula(divided_by_3) * 3 + gauss_formula(divided_by_5) * 5 - gauss_formula(divided_by_15) * 15
+
--- Problem_1/Test_Problem_1.py
+++ Problem_1/Test_Problem_1.py
@@ -1,5 +1,10 @@
-from Problem_1 import *
+from Problem_1 import gauss_formula, sum_multiples_3_and_5
+import pytest
 
-def test_sum_multiples():
-    assert sum_multiples_3_and_5(10) == 23
-    assert sum_multiples_3_and_5(100) == 2318
+@pytest.mark.parametrize("test_input, expected", [(10, 23), (100, 2318)])
+def test_sum_multiples(test_input, expected):
+    assert sum_multiples_3_and_5(test_input) == expected
+
+@pytest.mark.parametrize("test_input, expected", [(5, 15), (10, 55)])
+def test_gauss_formula(test_input, expected):
+    assert gauss_formula(test_input) == expected
